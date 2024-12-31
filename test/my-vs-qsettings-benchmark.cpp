c26877f218ec12dec01b51558ecbff81e70a7dc5
Wohlstand <admin@wohlnet.ru>
2025-01-01 02:38:55 +0300

    Update copyright year for 2025

--- test/my-vs-qsettings-benchmark.cpp
+++ test/my-vs-qsettings-benchmark.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2016-2024 Vitaliy Novichkov <admin@wohlnet.ru>
+Copyright (c) 2016-2025 Vitaliy Novichkov <admin@wohlnet.ru>
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the "Software"),
